467c0a05d526cc07c196f634b12cd1119dcac7f4
Omg Bebebe <omgbebebe@gmail.com>
2024-08-21 20:06:17 +0000

    set traceparent into nodes payload

--- src/knfunc/func.py
+++ src/knfunc/func.py
@@ -5,7 +5,7 @@ from mindwm.knfunc.decorators import iodoc, app
 logger = logging.getLogger(__name__)
 
 @iodoc
-async def func(
+async def io_context(
         iodocument: IoDocument,
         graph, 
         uuid: str,
@@ -15,23 +15,26 @@ async def func(
         session_id: str,
         pane_title: str):
 
-    user = graph.User(username=username).merge()
-    host = graph.Host(hostname=hostname).merge()
-    tmux = graph.Tmux(socket_path=socket_path).merge()
-    sess = graph.TmuxSession(name=session_id).merge()
-    pane = graph.TmuxPane(title=pane_title).merge()
+    traceparent = iodocument.traceparent
+    user = graph.User(username=username, traceparent=traceparent).merge()
+    host = graph.Host(hostname=hostname, traceparent=traceparent).merge()
+    tmux = graph.Tmux(socket_path=socket_path, traceparent=traceparent).merge()
+    sess = graph.TmuxSession(name=session_id, traceparent=traceparent).merge()
+    pane = graph.TmuxPane(title=pane_title, traceparent=traceparent).merge()
     iodoc = graph.IoDocument(
             uuid=uuid,
             input=iodocument.input,
             output=iodocument.output,
-            ps1=iodocument.ps1
+            ps1=iodocument.ps1,
+            traceparent=iodocument.traceparent,
+            tracestate=iodocument.tracestate
         ).create()
-    graph.UserHasHost(source=user, target=host).merge()
-    graph.HostHasTmux(source=host, target=tmux).merge()
-    graph.TmuxHasTmuxSession(source=tmux, target=sess).merge()
-    graph.UserHasTmux(source=user, target=tmux).merge()
-    graph.TmuxSessionHasTmuxPane(source=sess, target=pane).merge()
-    graph.TmuxPaneHasIoDocument(source=pane, target=iodoc).merge()
-    graph.IoDocumentHasUser(source=iodoc, target=user).merge()
+    graph.UserHasHost(source=user, target=host, traceparent=traceparent).merge()
+    graph.HostHasTmux(source=host, target=tmux, traceparent=traceparent).merge()
+    graph.TmuxHasTmuxSession(source=tmux, target=sess, traceparent=traceparent).merge()
+    graph.UserHasTmux(source=user, target=tmux, traceparent=traceparent).merge()
+    graph.TmuxSessionHasTmuxPane(source=sess, target=pane, traceparent=traceparent).merge()
+    graph.TmuxPaneHasIoDocument(source=pane, target=iodoc, traceparent=traceparent).merge()
+    graph.IoDocumentHasUser(source=iodoc, target=user, traceparent=traceparent).merge()
 
     logger.info(iodoc)
